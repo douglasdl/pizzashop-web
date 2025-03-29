b48faae71e31c8307a83a3945dcb0daa0ba1c018
Douglas Dias Leal <douglas_san@hotmail.com>
2025-03-29 16:49:49 +0900

    feat: add Profile update E2E test

--- web/src/api/mocks/update-profile-mock.ts
+++ web/src/api/mocks/update-profile-mock.ts
@@ -4,7 +4,7 @@ import { UpdateProfileBody } from '../update-profile'
 export const updateProfileMock = http.put<never, UpdateProfileBody>('/profile', async ({ request }) => {
   const { name } = await request.json()
 
-  if(name === 'Pizza Shop') {
+  if(name === 'The Pizza Shop') {
     return new HttpResponse(null, { status: 204 })
   }
 
---
+++ web/test/store-profile.e2e-spec.ts
@@ -0,0 +1,28 @@
+import { test, expect } from '@playwright/test';
+
+test('update profile successfully', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' });
+
+  await page.getByRole('button', { name: 'Pizza Shop' }).click()
+  
+  await page.getByRole('menuitem', { name: 'Perfil da Loja' }).click()
+  
+  await page.getByRole('textbox', { name: 'Nome' }).fill('The Pizza Shop')
+  await page.getByRole('textbox', { name: 'Descrição' }).fill('Another description')
+  
+  await page.getByRole('button', { name: 'Salvar' }).click()
+  
+  await page.waitForLoadState('networkidle')
+  
+  const toast = page.getByText('Perfil atualizado com sucesso!')
+
+  expect(toast).toBeVisible()
+  
+  await page.getByRole('button', { name: 'Close' }).click()
+
+  await page.waitForTimeout(250)
+
+  expect(page.getByRole('button', { name: 'The Pizza Shop' })).toBeVisible()
+
+  // await page.waitForTimeout(2000)
+});
